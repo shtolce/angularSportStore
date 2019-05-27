1edf5e811f4c73b6d99664f2ba8e4b0f104aa10e
shtolce <kimdimka@yandex.ru>
2019-05-28 00:29:12 +1000

    12

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from "../model/cart.model";
 @Component({
   selector: 'app-store',
   templateUrl: 'store.component.html'
@@ -9,7 +10,7 @@ export class StoreComponent {
   public selectedCategory =  null;
   public productsPerPage = 4;
   public selectedPage = 1;
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository, private cart: Cart) { }
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage
     return this.repository.getProducts(this.selectedCategory)
@@ -34,6 +35,10 @@ export class StoreComponent {
   get pageCount(): number {
     return Math.ceil(this.repository
       .getProducts(this.selectedCategory).length / this.productsPerPage);
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
   /*
   get pageNumbers(): number[] {
